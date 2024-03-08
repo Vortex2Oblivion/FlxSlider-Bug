ce40620e2b97a3b9b36f44d017e2356903a6ea39
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2024-03-08 14:36:39 -0500

    Init

---
+++ source/PlayState.hx
@@ -0,0 +1,16 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState
+{
+	public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
